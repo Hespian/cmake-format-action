1a9449f5b13412d5da8a6da5a254bf448f78009e
Demian Hespe <Hespian@users.noreply.github.com>
2022-08-11 14:33:19 +0200

    Use output of cmake-format as exit code

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,5 @@
 
 cd "$GITHUB_WORKSPACE" || exit 1
 
-find . \( -name '*.cmake' -o -name 'CMakeLists.txt' \) -exec cmake-format $* {} \;
+cmake-format $* $(find . -name '*.cmake' -o -name 'CMakeLists.txt') \;
 exit $?
